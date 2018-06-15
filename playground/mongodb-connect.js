5bc7921ae7eb8b73fe5387328985e242b0909607
Luis Osta <luisosta1999@gmail.com>
2018-06-15 13:51:50 -0500

    uncommneted code in mongodb-connect

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -10,28 +10,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
     const db = client.db('TodoApp');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert todo', err);
-    //     }
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if(err) {
+            return console.log('Unable to insert todo', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
+    db.collection('Users').insertOne({
+        name: 'Luis Osta',
+        age: 18,
+        location: 'Dallas'
+    }, (err,result) => {
+        if(err) {
+            return console.log('Unable to insert user', err);
+        }
 
-
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // })
-    // db.collection('Users').insertOne({
-    //     name: 'Luis Osta',
-    //     age: 18,
-    //     location: 'Dallas'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-
-    //     console.log(result.ops[0]._id.getTimestamp());
-    // })
+        console.log(result.ops[0]._id.getTimestamp());
+    })
 
     client.close();
 })
