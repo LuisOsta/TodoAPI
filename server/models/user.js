6f1241842028ad5d562050031a9ed0934797f37f
Luis Osta <luisosta1999@gmail.com>
2018-07-31 16:56:57 -0500

    add GET /users/me and auth middlware

--- server/models/user.js
+++ server/models/user.js
@@ -38,6 +38,7 @@ UserSchema.methods.toJSON = function () {
 
   return _.pick(userObj, ['_id', 'email'])
 }
+
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth'
@@ -50,6 +51,22 @@ UserSchema.methods.generateAuthToken = function () {
   })
 }
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123')
+  } catch (e) {
+    return Promise.reject()
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+}
 var User =mongoose.model('User', UserSchema)
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash')
 const {ObjectID} = require('mongodb')
+
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express();
 const port = process.env.PORT
@@ -121,6 +123,14 @@ app.post('/users', (req, res) => {
     res.status(400).send(e)
   })
 })
+
+
+// GET /users/me
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+})
+
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
